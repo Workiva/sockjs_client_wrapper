fabdc27d3ca81b92b46a039e96dd44e18030fbbf
Olesia Thoms <olesia.thoms@workiva.com>
2019-10-21 12:16:25 -0600

    new .travis.yaml setup

--- tool/test.sh
+++ tool/test.sh
@@ -8,7 +8,7 @@ SOCKJS_SERVER=$!
 sleep 2
 
 echo -e 'pub run build_runner test --release -- -P travis'
-pub run build_runner test --release -- -P travis
+pub run dart_dev test -- -P travis
 
 kill $SOCKJS_SERVER
 sleep 2
