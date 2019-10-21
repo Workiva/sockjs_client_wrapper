68000b03777812664cc53c43cc23f84ad5c7afdc
Olesia Thoms <olesia.thoms@workiva.com>
2019-10-21 11:40:17 -0600

    cleanup test.sh

--- tool/test.sh
+++ tool/test.sh
@@ -3,20 +3,12 @@
 # Fast fail the script on failures.
 set -e
 
-DART_VERSION=$(dart --version 2>&1)
-DART_2_PREFIX="Dart VM version: 2"
+node tool/server.js &
+SOCKJS_SERVER=$!
+sleep 2
 
-if [[ $DART_VERSION = $DART_2_PREFIX* ]]; then
-    node tool/server.js &
-    SOCKJS_SERVER=$!
-    sleep 2
+echo -e 'pub run build_runner test --release -- -P travis'
+pub run build_runner test --release -- -P travis
 
-    echo -e 'pub run build_runner test --release -- -P travis'
-    pub run build_runner test --release -- -P travis
-
-    kill $SOCKJS_SERVER
-    sleep 2
-else
-    echo -e 'pub run dart_dev test'
-    pub run dart_dev test
-fi
+kill $SOCKJS_SERVER
+sleep 2
