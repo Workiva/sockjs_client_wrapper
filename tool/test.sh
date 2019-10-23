565e3b0492b1a7d2eac0df6baabcc1697a3c5e87
Olesia Thoms <olesia.thoms@workiva.com>
2019-10-23 11:57:18 -0600

    remove extra separator

--- tool/test.sh
+++ tool/test.sh
@@ -8,7 +8,7 @@ SOCKJS_SERVER=$!
 sleep 2
 
 echo -e 'pub run dart_dev test -- -P travis'
-pub run dart_dev test -- -P travis
+pub run dart_dev test -P travis
 
 echo -e 'pub run dart_dev test --release -- -P travis'
 pub run dart_dev test --release -- -P travis
